74710c77fd0e590ddad9484d26372847abd566ec
Amin Latifkar <amin.latifkar@gmail.com>
2021-02-21 11:08:01 +0330

    Initial Commit

---
+++ docker-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o allexport
+source ./local.env
+set +o allexport
+
+set -o allexport
+source ./.env
+set +o allexport
+
+mkdir ../log
+mkdir ../extra
+mkdir ../mysql
+mkdir ../mysql/export
+mkdir ../mysql/import
+mkdir ../${DOCKER_PROJECT_NAME}
+
+
+if [ "$1" = "rebuild" ]
+then
+	docker-compose -f docker-compose.yml up --build -d 
+elif [ "$1" = "force-rebuild" ]
+then
+	docker-compose -f docker-compose.yml up --build --force-recreate -d
+else
+	docker-compose -f docker-compose.yml up -d
+fi
